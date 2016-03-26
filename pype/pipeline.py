1526084c6a3da1a25f4fbc29feea8603393cc980
mikezcc <zcc_zudiac@hotmail.com>
2016-03-25 22:59:48 -0400

    Added file for lab22

--- pype/pipeline.py
+++ pype/pipeline.py
@@ -1,8 +1,8 @@
 from .lexer import lexer
 from .parser import parser
 from .ast import *
-from .semantic_analysis import CheckSingleAssignment
-from .translate import SymbolTableVisitor
+from .semantic_analysis import CheckSingleAssignment, CheckSingleIOExpression, CheckUndefinedVariables
+from .translate import SymbolTableVisitor, LoweringVisitor
 
 class Pipeline(object):
   def __init__(self, source):
@@ -15,6 +15,15 @@ class Pipeline(object):
     ast = parser.parse(input, lexer=lexer)
     # Semantic analysis
     ast.walk( CheckSingleAssignment() )
-    # Translation
+    ast.walk( CheckSingleIOExpression() )
     syms = ast.walk( SymbolTableVisitor() )
-    return syms
+    ast.walk( CheckUndefinedVariables(syms) )
+
+    # Translation
+    ir = ast.mod_walk( LoweringVisitor(syms) )
+
+    # Optimization
+    ir.flowgraph_pass( AssignmentEllision() )
+    ir.flowgraph_pass( DeadCodeElimination() )
+
+    return ir
--- pype/symtab.py
+++ pype/symtab.py
@@ -34,3 +34,11 @@ class SymbolTable(object):
     else:
       self.T[scope]={sym.name:sym}
 
+  def lookupsym(self, sym, scope=None):
+    if scope is not None:
+      if sym in self.T[scope]:
+        return self.T[scope][sym]
+    if sym in self.T['global']:
+      return self.T['global'][sym]
+    return None
+
